c125331f7babeba212688e88e8b16de06c777961
GD138 <10gday@SWA-233-20>
2022-11-02 09:44:54 +0000

    Broken check

--- Subroutines1/Program.cs
+++ Subroutines1/Program.cs
@@ -28,9 +28,8 @@ while (Finished == false)
         Turn = true;
     }
     PrintBoard(board);
+    CheckRow(board, symbol);
 }
-PrintBoard(board);
-
 
 static void PrintBoard(string[,] b)
 {
@@ -52,3 +51,11 @@ static void UpdateBoard(string[,] b, int row, int col, string symbol)
     b[row, col] = symbol;
 }
 
+static bool CheckRow(string[,] b, string symbol)
+{
+    if (b[0,0] == "x" && b[1,0] == "x" && b[2,0] == "x" || b[0, 0] == "0" && b[1, 0] == "0" && b[2, 0] == "0")
+    {
+        Console.WriteLine("You Won!");
+    }
+    return true;
+}
